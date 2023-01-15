154703107f94464255bc5ab4165c6589cbb49301
Dawid Jewdokimow <dawid.jewdokimow@gmail.com>
2023-01-15 23:53:54 +0100

    sample tests - not finished yet

---
+++ app/src/androidTest/java/com/jewdokimow/hiltdemo/HiltSampleTest.kt
@@ -0,0 +1,69 @@
+package com.jewdokimow.hiltdemo
+
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.jewdokimow.hiltdemo.di.LotteriesGeneratorModule
+import com.jewdokimow.hiltdemo.di.LotteryAppModule
+import com.jewdokimow.hiltdemo.lotteries.LotteriesAppData
+import com.jewdokimow.hiltdemo.lotteries.models.DailyLottery
+import com.jewdokimow.hiltdemo.lotteries.models.UserData
+import com.jewdokimow.hiltdemo.lotteries.repositories.ILotteryRepository
+import com.jewdokimow.hiltdemo.lotteries.utilities.ILotteriesGeneratorUtil
+import dagger.hilt.android.testing.BindValue
+import dagger.hilt.android.testing.HiltAndroidRule
+import dagger.hilt.android.testing.HiltAndroidTest
+import dagger.hilt.android.testing.UninstallModules
+import junit.framework.TestCase.assertEquals
+import org.junit.Before
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+import java.time.LocalDate
+import javax.inject.Inject
+
+@HiltAndroidTest
+@RunWith(AndroidJUnit4::class)
+@UninstallModules(LotteryAppModule::class)
+class HiltSampleTest {
+
+
+
+    @get:Rule
+    var hiltRule = HiltAndroidRule(this)
+
+    @BindValue
+    val appData: LotteriesAppData = LotteriesAppData(
+        userLotteries = listOf(
+            DailyLottery(
+                LocalDate.of(1995, 2, 21),
+                LocalDate.of(1995, 2, 21),
+                LocalDate.of(1995, 2, 21),
+                LocalDate.of(1995, 2, 21)
+            )
+        ),
+        userData = UserData("tester", LocalDate.of(1995, 2, 21))
+    )
+
+
+    @Inject
+    lateinit var dailyLottery: ILotteryRepository<DailyLottery>
+
+    @Before
+    fun init() {
+        hiltRule.inject()
+    }
+
+    @Test
+    fun sampleTest() {
+        assertEquals(
+            dailyLottery.getLotteriesAvailableForUser(),
+            listOf(
+                DailyLottery(
+                    LocalDate.of(1995, 2, 21),
+                    LocalDate.of(1995, 2, 21),
+                    LocalDate.of(1995, 2, 21),
+                    LocalDate.of(1995, 2, 21)
+                )
+            )
+        )
+    }
+}
--- app/src/main/java/com/jewdokimow/hiltdemo/di/LotteriesGeneratorModule.kt
+++ app/src/main/java/com/jewdokimow/hiltdemo/di/LotteriesGeneratorModule.kt
@@ -1,29 +1,28 @@
 package com.jewdokimow.hiltdemo.di
 
 import com.jewdokimow.hiltdemo.lotteries.LotteriesAppData
-import com.jewdokimow.hiltdemo.lotteries.models.UserData
 import com.jewdokimow.hiltdemo.lotteries.utilities.ILotteriesGeneratorUtil
 import com.jewdokimow.hiltdemo.lotteries.utilities.LotteriesGeneratorUtil
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
-import java.time.LocalDate
-import java.util.Random
+import java.util.*
 import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
-class LotteryAppModule {
+class LotteriesGeneratorModule {
 
     @Singleton
     @Provides
-    fun provideLotteriesAppData(): LotteriesAppData {
-        return LotteriesAppData(
-            userData = UserData(
-                name = "Dawid",
-                birthday = LocalDate.of(1993, 7, 27)
-            )
-        )
+    fun provideRandom(): Random {
+        return Random(System.currentTimeMillis())
+    }
+
+    @Singleton
+    @Provides
+    fun provideLotteriesGeneratorUtil(data: LotteriesAppData): ILotteriesGeneratorUtil {
+        return LotteriesGeneratorUtil(data)
     }
 }
